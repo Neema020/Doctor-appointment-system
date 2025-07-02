c84d3aa153f1ffc37bef977c3778f12e6775de2f
Neema020 <dolmaghising02@gmail.com>
2025-07-02 15:06:18 +0545

    Create Appointment Booking Functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary'
+import doctorModel from '../models/doctorModel.js'
+import appointmentModel from '../models/appointmentModel.js'
 
 
 // api to register user
@@ -146,7 +148,68 @@ const updateProfile = async (req,res) => {
 
 }
 
+// API TO  book appointment
+
+const bookAppointment = async (req, res) => {
+
+    try{
+        const {docId,slotDate,slotTime} = req.body
+        const userId = req.userId;
+
+        const docData = await doctorModel.findById(docId).select('-password')
+
+        if(!docData.available){
+            return res.json({success:false,message:"Doctor not available."})
+        }
+
+        let slots_booked = docData.slots_booked
+
+        // checking for slots availability
+        if(slots_booked[slotDate]){
+            if(slots_booked[slotDate].includes(slotTime)){
+               return res.json({success:false,message:"Slot not available"}) 
+            }else{
+                slots_booked[slotDate].push(slotTime)
+            }
+        }else{
+            slots_booked[slotDate] = []
+            slots_booked[slotDate].push(slotTime)
+        }
+
+        const userData = await userModel.findById(userId).select('-password')
+
+        delete docData.slots_booked
+
+        const appointmentData = {
+            userId,
+            docId,
+            slotDate,
+            slotTime,
+            userData,
+            docData,
+            amount:docData.fees,
+            date:Date.now(),
+        }
+        const newAppointment = new appointmentModel(appointmentData)
+        await newAppointment.save()
+
+        //save new slots data in DocData
+
+        await doctorModel.findByIdAndUpdate(docId,{slots_booked})
+
+        res.json({success:true,message:"Appointment Booked Successfully"})
 
 
 
-export {registerUser,loginUser,getProfile,updateProfile}
+
+
+    }catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
+}
+
+
+
+
+export {registerUser,loginUser,getProfile,updateProfile,bookAppointment}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -74,7 +74,7 @@ const AppContextProvider = (props) => {
 
 
   const value = {
-    doctors,
+    doctors,getDoctorsData,
     currencySymbol,
     token,setToken,
     backendUrl,
