1292505102f7685ebfb8fd2ee25cd86fa3761a42
Neema020 <dolmaghising02@gmail.com>
2025-07-03 21:24:00 +0545

    Create doctor panel ( doctor dashboard )

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,10 +1,18 @@
+//import { set } from "mongoose";
+import { useState } from "react";
 import { createContext } from "react";
 
 export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props) => {
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [dToken, setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
+
     const value = {
+        dToken,setDToken,
+        backendUrl
 
 
     }
